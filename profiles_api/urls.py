9598a3bff1e02c6329890c35a3aaceebdf0eb18a
kctay2022 <tay.kok.chuan@gmail.com>
2022-04-10 15:51:22 +0800

    Added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
@@ -10,5 +9,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
